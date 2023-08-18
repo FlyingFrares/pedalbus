47833acb3c7031c41c8b768fa67aaec032448907
Nicolò Salvi <nicolo.salvi@edu.unife.it>
2023-08-18 04:51:23 +0200

    Aggiunto modulo per la generazione dei grafi

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import data_parser
 import os
+import graph
 
 test_file = "pedibus_10"  # edit this if you do not want to pass a command line arg.
 
@@ -7,4 +8,6 @@ file_path = "instances" + os.path.sep + test_file + ".dat"
 
 data = data_parser.get_data(file_path)
 
-print(data)
+gr = graph.Graph(data)
+
+print(gr.get_ordered_vector([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
