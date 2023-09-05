20a4663d4f7299886e2b5b1ae534344127f443ce
Nicolò Salvi <nicolo.salvi@edu.unife.it>
2023-09-05 10:42:58 +0200

    Creato plot per istanza n=10, hardcoded

--- test/plot.py
+++ test/plot.py
@@ -1,36 +1,28 @@
-import matplotlib.pyplot as plt
 import networkx as nx
+import matplotlib.pyplot as plt
 
-# X and Y coordinates of the 10 points
-x_coordinates = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-y_coordinates = [5, 3, 8, 4, 9, 1, 6, 2, 7, 10]
-
-# Distances between points
-distances = [2.5, 1.8, 3.2, 1.5, 2.7, 3.0, 2.2, 2.9, 1.3, 2.0]
-
-# Create the graph
+# Creazione di un grafo
 G = nx.Graph()
 
-# Add nodes to the graph
-for i in range(len(x_coordinates)):
-    G.add_node(i, pos=(x_coordinates[i], y_coordinates[i]))
+# Aggiungi i collegamenti e le rispettive distanze
+edges = [
+    (0, 10, 5.0),
+    (0, 8, 6.4),
+    (1, 5, 7.6),
+    (2, 9, 7.6),
+    (4, 8, 9.2),
+    (2, 4, 12.2),
+    (1, 3, 12.4),
+    (0, 3, 15.0),
+    (5, 7, 26.2),
+    (5, 6, 27.8),
+]
 
-# Add edges with distances as attributes
-for i in range(len(x_coordinates)):
-    for j in range(i + 1, len(x_coordinates)):
-        distance = distances[i] if i < j else distances[j]  # Choose the smaller distance between the two points
-        G.add_edge(i, j, weight=distance)
+for edge in edges:
+    G.add_edge(edge[0], edge[1], weight=edge[2])
 
-# Get node positions
-node_positions = nx.get_node_attributes(G, 'pos')
-
-# Create the plot
-plt.figure(figsize=(8, 6))
-nx.draw(G, pos=node_positions, with_labels=True, node_size=3000, node_color='lightblue', font_size=10, font_color='black')
-
-# Add distance labels on the edges
-edge_labels = nx.get_edge_attributes(G, 'weight')
-nx.draw_networkx_edge_labels(G, pos=node_positions, edge_labels=edge_labels)
-
-# Show the plot
+# Disegna il grafo
+pos = nx.spring_layout(G)  # Layout del grafo
+labels = nx.get_edge_attributes(G, 'weight')
+nx.draw(G, pos, with_labels=True, node_size=500, node_color='lightblue')
 plt.show()
