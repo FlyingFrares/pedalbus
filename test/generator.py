42829ef8d3c78450d89d99392eb6a7b871306626
Nicolò Salvi <nicolo.salvi@edu.unife.it>
2023-09-07 18:35:24 +0200

    Generate istanze fresche

--- test/generator.py
+++ test/generator.py
@@ -9,14 +9,14 @@ coordX = {i: random.randint(0, 100) for i in range(n + 1)}
 coordY = {i: random.randint(0, 100) for i in range(n + 1)}
 
 # Scrittura nel file AMPL
-with open(f'pedibus_{n}.dat', 'w') as f:
+with open(f'pedalbus_{n}.dat', 'w') as f:
     f.write('param n := {}\n;\n\n'.format(n))
     f.write('param delta := {}\n;\n\n'.format(delta))
-    f.write('param coordX [*] :=\n')
+    f.write('param coordX :=\n')
     for i in range(n + 1):
         f.write('{:<2}  {:>2}\n'.format(i, coordX[i]))
     f.write(';\n\n')
-    f.write('param coordY [*] :=\n')
+    f.write('param coordY :=\n')
     for i in range(n + 1):
         f.write('{:<2}  {:>2}\n'.format(i, coordY[i]))
     f.write(';\n')
